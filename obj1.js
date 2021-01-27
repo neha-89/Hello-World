c15307adef7a2a84a7eb0aae509506ebf3da7b2d
Neha Sontakke <wasnik.neha@gmail.com>
2021-01-27 13:47:08 +0530

    changes done

--- obj1.js
+++ obj1.js
@@ -1,1 +1,18 @@
-let arr1 = [1,2,3,4,5];
+async function hello(){
+    return "hello";
+}
+console.log(hello());
+
+let hello1 = async function(){
+    return "hello";
+
+}
+console.log(hello1());
+
+hello1().then(console.log);
+
+
+async function hello(){
+    return greeting = await Promise.resolve("hello");
+}
+hello().then(console.log);
